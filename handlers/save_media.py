a76ac433423ef57e1afe4b95179085c70f956079
Tousif2003 <135956946+Tousif2003@users.noreply.github.com>
2024-02-03 13:33:08 +0530

    Update save_media.py

--- handlers/save_media.py
+++ handlers/save_media.py
@@ -54,9 +54,7 @@ async def save_batch_media_in_channel(bot: Client, editable: Message, message_id
             f"**Batch Files Stored in my Database!**\n\nHere is the Permanent Link of your files: {share_link} \n\n"
             f"Just Click the link to get your files!",
             reply_markup=InlineKeyboardMarkup(
-                [[InlineKeyboardButton("Open Link", url=share_link)],
-                 [InlineKeyboardButton("Bots Channel", url="https://t.me/TeleRoidGroup"),
-                  InlineKeyboardButton("Support Group", url="https://t.me/TeleRoid14")]]
+                [[InlineKeyboardButton("Open Link", url=share_link)]]
             ),
             disable_web_page_preview=True
         )
@@ -93,9 +91,7 @@ async def save_media_in_channel(bot: Client, editable: Message, message: Message
             f"Here is the Permanent Link of your file: {share_link} \n\n"
             "Just Click the link to get your file!",
             reply_markup=InlineKeyboardMarkup(
-                [[InlineKeyboardButton("Open Link", url=share_link)],
-                 [InlineKeyboardButton("Bots Channel", url="https://t.me/TeleRoidGroup"),
-                  InlineKeyboardButton("Support Group", url="https://t.me/TeleRoid14")]]
+                [[InlineKeyboardButton("Open Link", url=share_link)]]
             ),
             disable_web_page_preview=True
         )
