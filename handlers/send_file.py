12296d01dbcfb21cfeca2e552b80069ff7496279
Tousif2003 <135956946+Tousif2003@users.noreply.github.com>
2024-02-05 11:54:13 +0530

    Update send_file.py

--- handlers/send_file.py
+++ handlers/send_file.py
@@ -7,12 +7,14 @@ from handlers.helpers import str_to_b64
 
 
 async def reply_forward(message: Message, file_id: int):
-#    try:
-#        m=await message.reply_text(".")
-#        await asyncio.sleep(1)
+    try:
+        await message.reply_text(
+            f"**Fɪʟᴇꜱ Iꜱ Rᴇᴀᴅʏ...**\n",
+            disable_web_page_preview=True, quote=True)
     except FloodWait as e:
+        await asyncio.sleep(e.value)
         await reply_forward(message, file_id)
-#        await m.delete()
+
 
 async def media_forward(bot: Client, user_id: int, file_id: int):
     try:
@@ -23,17 +25,16 @@ async def media_forward(bot: Client, user_id: int, file_id: int):
             return await bot.forward_messages(chat_id=user_id, from_chat_id=Config.DB_CHANNEL,
                                               message_ids=file_id)
     except FloodWait as e:
-        await asyncio.sleep(1)
-        await loading_message.delete()
+        await asyncio.sleep(e.value)
         return media_forward(bot, user_id, file_id)
 
 
 async def delete_file(file_id: int):
-    await asyncio.sleep(300)  # wait for 30 minutes
+    await asyncio.sleep(1800)  # wait for 30 minutes
 
 async def send_media_and_reply(bot: Client, user_id: int, file_id: int):
     sent_message = await media_forward(bot, user_id, file_id)
     await reply_forward(message=sent_message, file_id=file_id)
     asyncio.create_task(delete_file(file_id))  # schedule the file deletion task
-    await asyncio.sleep(0.1)
-   # await loading_message.delete()
+    await asyncio.sleep(2)
+   
