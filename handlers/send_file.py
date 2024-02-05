24adc1b8618a0aa859c4439c912bf195ea44b33e
Tousif2003 <135956946+Tousif2003@users.noreply.github.com>
2024-02-05 10:18:42 +0530

    Update send_file.py

--- handlers/send_file.py
+++ handlers/send_file.py
@@ -6,14 +6,14 @@ from pyrogram.errors import FloodWait
 from handlers.helpers import str_to_b64
 
 
-async def reply_forward(message: Message, file_id: int):
-    try:
-        await message.reply_text(
-            f"**Files will be Deleted After 30 min ⏰**\n",
-            disable_web_page_preview=True, quote=True)
-    except FloodWait as e:
-        await asyncio.sleep(e.value)
-        await reply_forward(message, file_id)
+#async def reply_forward(message: Message, file_id: int):
+#    try:
+#        await message.reply_text(
+#            f"**Files will be Deleted After 30 min ⏰**\n",
+#            disable_web_page_preview=True, quote=True)
+#    except FloodWait as e:
+#        await asyncio.sleep(e.value)
+#        await reply_forward(message, file_id)
 
 
 async def media_forward(bot: Client, user_id: int, file_id: int):
@@ -29,11 +29,11 @@ async def media_forward(bot: Client, user_id: int, file_id: int):
         return media_forward(bot, user_id, file_id)
 
 
-async def delete_file(file_id: int):
-    await asyncio.sleep(300)  # wait for 30 minutes
+#async def delete_file(file_id: int):
+#    await asyncio.sleep(300)  # wait for 30 minutes
 
 async def send_media_and_reply(bot: Client, user_id: int, file_id: int):
     sent_message = await media_forward(bot, user_id, file_id)
     await reply_forward(message=sent_message, file_id=file_id)
     asyncio.create_task(delete_file(file_id))  # schedule the file deletion task
-    await asyncio.sleep(1)
+    await asyncio.sleep(0.1)
