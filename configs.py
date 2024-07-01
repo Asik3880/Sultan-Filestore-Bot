00b310e0fdbca28286a1c3b6a77c371ecfc4918a
Asik3880 <141816024+Asik3880@users.noreply.github.com>
2024-07-01 14:51:42 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -4,15 +4,15 @@ import os
 
 
 class Config(object):
-	API_ID = int(os.environ.get("API_ID", "0"))
-	API_HASH = os.environ.get("API_HASH")
-	BOT_TOKEN = os.environ.get("BOT_TOKEN")
-	BOT_USERNAME = os.environ.get("BOT_USERNAME")
-	DB_CHANNEL = int(os.environ.get("DB_CHANNEL", "-100"))
-	BOT_OWNER = int(os.environ.get("BOT_OWNER", "1445283714"))
-	DATABASE_URL = os.environ.get("DATABASE_URL")
+	API_ID = int(os.environ.get("API_ID", "29798427"))
+	API_HASH = os.environ.get("API_HASH", "b273dd0246a0e0b369c4266a3e6deb2e")
+	BOT_TOKEN = os.environ.get("BOT_TOKEN", "6811058101:AAGbjbpNpLM00Mkws14qyMt26i3uxQ6S8Cg")
+	BOT_USERNAME = os.environ.get("BOT_USERNAME", "Mini_netflix_filestore_bot")
+	DB_CHANNEL = int(os.environ.get("DB_CHANNEL", "-1002068937373"))
+	BOT_OWNER = int(os.environ.get("BOT_OWNER", "1395730131"))
+	DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://Kaif2001:Kaif2001@cluster0.bbnjgsn.mongodb.net/?retryWrites=true&w=majority")
 	UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "")
-	LOG_CHANNEL = os.environ.get("LOG_CHANNEL", None)
+	LOG_CHANNEL = os.environ.get("LOG_CHANNEL", "-1002116670104")
 	BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "1234567890").split())
 	FORWARD_AS_COPY = bool(os.environ.get("FORWARD_AS_COPY", True))
 	BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", False))
